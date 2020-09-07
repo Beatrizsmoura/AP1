33455b247c2d83ce16466684f07b573696504101
Beatrizsmoura <60660117+Beatrizsmoura@users.noreply.github.com>
2020-09-07 17:36:29 -0400

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		//questao 02
+		System.out.println("###### PESQUISADORES ######" );
+		System.out.println("MATRIC. NOME		SAL. BASE	SAL.BRUTO	CATEGORIA" );
+		System.out.println("------- ----------- 	----------	----------	----------");
+		
+		
+		//questao 03
+		System.out.println("###### ALOCAÇOES ######" );
+		System.out.println("PESQUISADOR	PROJETO 	 DATA ENTRADA" );
+		System.out.println("-----------	--------------	 -------------	");
+
+	}
+}
