83459c86b652f2a8a19ba521994a3912d6be7c30
Beatrizsmoura <60660117+Beatrizsmoura@users.noreply.github.com>
2020-09-07 16:44:43 -0400

    Add files via upload
    adicionado alocaçao

--- InstitutoPesquisa.java
+++ InstitutoPesquisa.java
@@ -16,7 +16,7 @@ public class InstitutoPesquisa {
 		this.alocacoes = new ArrayList<Alocacao>();
 		
 		Projeto p1 = new Projeto("Robo domestico", 100000.0, 48, new Data(1, 1, 2007));
-		Projeto p2 = new Projeto("Motor ‡ vento", 200000.0, 15, new Data(2, 2, 2007));
+		Projeto p2 = new Projeto("Motor √† vento", 200000.0, 15, new Data(2, 2, 2007));
 		Projeto p3 = new Projeto("Playstation Wii", 300000.0, 24, new Data(3, 3, 2007));
 		Projeto p4 = new Projeto("Teletransporte", 400000.0, 36, new Data(4, 4, 2007));
 		
@@ -24,12 +24,34 @@ public class InstitutoPesquisa {
 		projetos.add(p2);
 		projetos.add(p3);
 		projetos.add(p4);
+				
+		Senior pesquisador1 = new Senior( "Casc√£o",11, 1000.0);
+		Assistente pesquisador2 = new Assistente("Cebolinha",22, 1000.0);
+		Assistente pesquisador3 = new Assistente("M√¥nica",33, 1200.0);
+		Pleno pesquisador4 = new Pleno("Magali",44, 1200.0);
+		Senior pesquisador5 = new Senior("Chico Bento",55,1400.0);
 		
-		// Precisa instaciar as classes correspodentes - num sei como faz		
-		Senior s = new Senior(11, "Casc„o", 1000.0);
-		pesquisadores.add(s);
+		pesquisadores.add(pesquisador1);
+		pesquisadores.add(pesquisador2);
+		pesquisadores.add(pesquisador3);
+		pesquisadores.add(pesquisador4);
+		pesquisadores.add(pesquisador5);
 		
-		alocacoes.add(new Alocacao(s, p1, new Data(4, 8, 2020)));
+	
+		
+		alocacoes.add(new Alocacao(pesquisador1, p1, new Data(4, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador1, p3, new Data(5, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador1, p4, new Data(6, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador2, p1, new Data(7, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador2, p3, new Data(8, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador3, p1, new Data(9, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador3, p2, new Data(10, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador3, p3, new Data(11, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador4, p4, new Data(12, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador5, p3, new Data(13, 8, 2020)));
+		alocacoes.add(new Alocacao(pesquisador5, p4, new Data(14, 8, 2020)));
+		
+		
 	}
 	
 	public ArrayList<Pesquisador> getPesquisadores(){
